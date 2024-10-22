9efa4df8f30a7593fb27447a22d373a4f1baa8a6
JGONZALEZ <jgonzalez@servicioexterno.bancocaminos.es>
2024-10-23 01:54:57 +0200

    [CD-0002] jwt.

--- service/src/main/java/es/zed/infrastructure/controller/PokeApiController.java
+++ service/src/main/java/es/zed/infrastructure/controller/PokeApiController.java
@@ -7,7 +7,6 @@ import lombok.RequiredArgsConstructor;
 import org.springframework.http.MediaType;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
-import org.springframework.web.bind.annotation.RequestHeader;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
